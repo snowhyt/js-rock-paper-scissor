49236b7c36fff50421cf41bb3fa7d21393c4b932
snowhyt <oliverglorioso.44@gmail.com>
2024-08-10 08:56:21 +0800

    add getPlayerchoice and playRound

--- RPS.js
+++ RPS.js
@@ -20,10 +20,63 @@ function getComputerChoice() {
   
 }
 //just to get the output
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
 
 //getting player answer
 
 function getPlayerChoice(){
+ let count = true;
+ while(count){
+
+
+ let playerMove = prompt("Choose ROCK, PAPER, SCISSOR")
+ 
+ playerMove = playerMove.toUpperCase();
+ 
+ 
+ if (playerMove === "ROCK"){
+   return playerMove;
+ } 
+  else if (playerMove === "PAPER"){
+   return playerMove;
+ } 
+else if (playerMove === "SCISSOR") {
+   return playerMove;
+ }
+ else{
+   count = true;
+   alert("The word input "+playerMove+" is not valid. Please try again.");
+   
+ }
+}
+
+}
+//console.log("The answer is "+getPlayerChoice());
+
+//comparison of answers; computer vs player
+function playRound(playerMove,computerMove){
+  let playerScore;
+  let computerScore;
+  
+  for(let round = 0; round < 5; round++){
+    
+  if(playerMove === "ROCK" && computerMove === "SCISSOR"){
+    return alert
+  } 
+   else if (playerMove === "PAPER" && computerMove === "ROCK") {
+  
+   }
+    else if (playerMove === "SCISSOR" && computerMove === "PAPER") {
+   
+    }
+    else{
+      
+    }
+  }
   
 }
+
+const playerMove = getPlayerChoice();
+const computerMove = getComputerChoice();
+
+playRound(playerMove, computerMove);
